ca9e16b13ac07bf2d0e97d676178a95b89fa875a
sultaAann <sultandjavgashev@gmail.com>
2023-06-06 12:03:13 +0500

    ok

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,22 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file){
+        String[] res = new String[4];
+        try (java.io.FileReader fileReader = new java.io.FileReader(file); BufferedReader rd = new BufferedReader(fileReader)) {
+            int i = 0;
+            while (rd.ready()) {
+                res[i] = (rd.readLine()).split(":")[1].trim();
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile(res[0], Integer.parseInt(res[1]), res[2], Long.parseLong(res[3]));
     }
 }
